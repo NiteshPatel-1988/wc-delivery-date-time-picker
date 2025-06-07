c04d8c2b8ace7ea2eb10f5c0938443263182b926
nitesh Patel <1988niteshpatel@gmail.com>
2025-06-07 16:13:51 +0530

    Initial commit - WooCommerce Delivery Date & Time Picker

---
+++ wc-delivery-date-time-picker.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: WC Delivery Date & Time Picker
+ * Description: Allow customers to choose delivery date and time during WooCommerce checkout.
+ * Version: 1.0
+ * Author: Nitesh Patel
+ */
+
+if (!defined('ABSPATH')) exit;
+
+// Include core functionality
+//require_once plugin_dir_path(__FILE__) . 'includes/class-wc-delivery-date-picker.php';
